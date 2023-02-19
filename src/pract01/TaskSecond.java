1b68cad310a94945c375732d4fbd86fb8c3b0cae
Yuliia Mokina <iuliia.mokina@gmail.com>
2023-02-19 12:31:19 +0200

    add homework and practical tasks
    Please enter the commit message for your changes. Lines starting

---
+++ src/pract01/TaskSecond.java
@@ -0,0 +1,28 @@
+package pract01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class TaskSecond {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Input first number:");
+        int firstnumber = Integer.parseInt(br.readLine());
+        System.out.println("Input second number:");
+        int secondnumber = Integer.parseInt(br.readLine());
+        System.out.println("Input third number:");
+        int thirddnumber = Integer.parseInt(br.readLine());
+
+        int[] numbers = {firstnumber, secondnumber, thirddnumber};
+        float sum = 0;
+
+        for(int num:numbers)
+            sum += num;
+
+        float average = sum / numbers.length;
+
+        System.out.println("Average is "+average);
+
+    }
+}
