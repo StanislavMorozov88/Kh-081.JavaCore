7eac720dc5a2c053e1e578b20a80493e551920fd
Roman <rzozulya1997@gmail.com>
2023-02-22 22:50:10 +0200

    add homework

---
+++ lesson3/Homework3/src/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int birthYear;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return 2023-birthYear;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
