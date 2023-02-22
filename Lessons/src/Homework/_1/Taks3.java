cc00a9e6eee5a8a8a478fc024b4cefd476b13047
Oyoxsi <pavlotkatch@gmail.com>
2023-02-22 22:56:25 +0200

    adding homework

---
+++ Lessons/src/Homework/_1/Taks3.java
@@ -0,0 +1,44 @@
+package Homework._1;
+
+import java.util.Scanner;
+
+public class Taks3 {
+
+	public static void main(String[] args) {
+		/*
+		 * Phone calls from three different countries cost c1, c2 and c3 conventional
+		 * units per minute. Talks lasted t1, t2 and t3 minutes. What is the cost of
+		 * each call? What is the total cost? Input all source data from console, make
+		 * calculations and output to the console
+		 */
+		
+		float[] cost = new float[3];
+		float[] time = new float[3];
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Hello, enter below price of calls (in c.u.) from different countries \n"
+				+ "and their duration (in minutes): ");
+		
+		System.out.print("Price fom Italy: ");
+		cost[0]=input.nextFloat();
+		System.out.print("Duration: ");
+		time[0]=input.nextFloat();
+		
+		System.out.print("Price fom Japan: ");
+		cost[1]=input.nextFloat();
+		System.out.print("Duration: ");
+		time[1]=input.nextFloat();
+		
+		System.out.print("Price fom UK: ");
+		cost[2]=input.nextFloat();
+		System.out.print("Duration: ");
+		time[2]=input.nextFloat();
+		
+		System.out.println("You have to pay a bill of: \n"
+				+ time[0]*cost[0] + " c.u. for a call from Italy,\n"
+				+ time[1]*cost[1] + " c.u. for a call from Japan,\n"
+				+ time[2]*cost[2] + " c.u. for a call from the UK.");
+
+	}
+
+}
---
+++ Lessons/src/Homework/_1/Task2.java
@@ -0,0 +1,23 @@
+package Homework._1;
+
+import java.util.Scanner;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+//		Define String variables name and address. Output question "What is your name?"
+//		Read the value name and output next question: “Where are you live, (name)?". Read
+//		address and write whole information 
+		
+		String name;
+		String address;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("What is your name?");
+		name = input.nextLine();
+		System.out.println("Where are you live, " + name);
+		address = input.nextLine();
+		System.out.println("Nice to meet you, " + name + " from " + address);
+	}
+
+}
