8ad111d72165fbfa87d2828794aa808fa717a8f1
iterepa <wizmond705@gmail.com>
2023-02-23 12:07:21 +0200

    Created class Person, created class Main, added code to this classes

---
+++ Lesson3/src/HomeTask/Main.java
@@ -0,0 +1,33 @@
+package HomeTask;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person p1 = new Person("Ivan", "Terepa", 2001);
+        Person p2 = new Person("Andrew", "Hoking", 1987);
+        Person p3 = new Person();
+        Person p4 = new Person("Anna", "Marsh");
+        Person p5 = new Person("Alexandra", "Galych");
+
+        p3.input("Ksenia", "Koval", 1999);
+        p4.setBirthYear(2004);
+        p5.setBirthYear(1976);
+        p2.changeName("Andrew", "Schwitz");
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+        System.out.println(p4);
+        System.out.println(p5);
+        System.out.println(p1.getAge());
+        System.out.println(p2.getAge());
+        System.out.println(p3.getAge());
+        System.out.println(p4.getAge());
+        System.out.println(p5.getAge());
+
+
+    }
+}
---
+++ Lesson3/src/HomeTask/Person.java
@@ -0,0 +1,75 @@
+package HomeTask;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int birthYear;
+
+    private static final int CURRENTYEAR = 2023;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName, int birthYear) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public int getAge(){
+        return (CURRENTYEAR - birthYear);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthYear=" + birthYear +
+                '}';
+    }
+
+    public void input(String firstName, String lastName, int birthYear){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
+    }
+
+    private void output(){
+        System.out.println(toString());
+    }
+
+    public void changeName(String fn, String ln){
+        this.firstName = fn;
+        this.lastName = ln;
+    }
+}
