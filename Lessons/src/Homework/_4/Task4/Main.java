4f99a674bd905e1254ec863d408c04dff4d12437
Oyoxsi <pavlotkatch@gmail.com>
2023-03-02 00:12:54 +0200

    complete practicak task 4

--- Lessons/src/Homework/_4/Task4/Main.java
+++ Lessons/src/Homework/_4/Task4/Main.java
@@ -1,4 +1,32 @@
 package Homework._4.Task4;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p[] = new Product[4];
+        p[0] = new Product("снікерс", 57.3, 12);
+        p[1] = new Product("марс", 63.2, 24);
+        p[2] = new Product("баунті", 72.5, 13);
+        p[3] = new Product("твікс", 79.15, 20);
+
+        double maxPrice=0;
+        int maxQuantity=0;
+        String mostExpensiveProduct = null;
+
+        for (Product product : p) {
+            if (maxPrice < product.getPrice()) {
+                maxPrice = product.getPrice();
+                mostExpensiveProduct = product.getName();
+            }
+        }
+        System.out.println("Найбільша ціна у батончику "+ mostExpensiveProduct+ " - "+ maxPrice+ " грн.");
+
+        for (Product product : p) {
+            if (maxQuantity < product.getQuantity()) {
+                maxQuantity = product.getQuantity();
+                mostExpensiveProduct = product.getName();
+            }
+        }
+        System.out.println("Найбільша кількість товару у батончику "+ mostExpensiveProduct+ " - "+ maxQuantity+ " шт.");
+
+    }
 }
--- Lessons/src/Homework/_4/Task4/Product.java
+++ Lessons/src/Homework/_4/Task4/Product.java
@@ -1,4 +1,41 @@
 package Homework._4.Task4;
 
 public class Product {
+   private String name;
+   private double price;
+   private int quantity;
+
+    public Product() {
+    }
+    public Product(String name) {
+        this.name = name;
+    }
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
