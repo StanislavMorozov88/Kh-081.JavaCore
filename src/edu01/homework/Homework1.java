f4e27be9fd4a8d605a9926c1c59a8611ea95a552
Yaroslav Korchynskyi <yaroslavkorchynskiy@gmail.com>
2023-03-15 15:05:52 +0200

    test

---
+++ src/edu01/homework/Homework1.java
@@ -0,0 +1,19 @@
+package edu01.homework;
+
+/*
+Flowerbed
+ */
+
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        Scanner scanner1 = new Scanner(System.in);
+        int radius = scanner1.nextInt();
+        double pi = 3.14;
+        double perimetr = 2 * radius * pi;
+        double area = radius * radius * pi;
+        System.out.println(perimetr);
+        System.out.println(area);
+    }
+}
