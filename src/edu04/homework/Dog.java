a7ea445fc600278f93d665d1e5e14ea5f086eb1f
Yaroslav Korchynskyi <yaroslavkorchynskiy@gmail.com>
2023-03-16 18:41:26 +0200

    homework to 4 theme

---
+++ src/edu04/homework/Dog.java
@@ -0,0 +1,13 @@
+package edu04.homework;
+
+public class Dog {
+    private String name;
+    private Breed breed;
+    private int age;
+    public Dog(String name, Breed breed, int age){
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+}
