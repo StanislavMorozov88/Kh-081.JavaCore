f4735d6f410f8ece846475289fd16c784c56d6da
Olena Stankevych <yelenastankevych@gmail.com>
2023-03-24 23:59:54 +0200

    made some changes in home task 03 (add input currency from console)

--- IdeaProjects/src/lesson11/home/Home03.java
+++ IdeaProjects/src/lesson11/home/Home03.java
@@ -1,21 +1,41 @@
 package lesson11.home;
 
+import java.util.Scanner;
+import java.util.StringJoiner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Home03 {
+    private static final Scanner SC = new Scanner(System.in);
     private static final String US_PATTERN = "\\$\\d+\\.\\d{2}";
 
-    public static void printOccurrences(String usOccurrences) {
+    public static String printUsCurrency() {
+        System.out.println("Enter US Currency: ");
+        String currency = SC.nextLine();
+        System.out.println("You enter: " + currency);
+        return currency;
+    }
+
+    public static void printOccurrences(String currency) {
         Pattern p = Pattern.compile(US_PATTERN);
-        Matcher m = p.matcher(usOccurrences);
+        Matcher m = p.matcher(currency);
+        StringJoiner stringJoiner = new StringJoiner(", ");
         while (m.find()){
-            System.out.println(String.join(", ", usOccurrences.substring(m.start(),m.end())));
+            stringJoiner.add(currency.substring(m.start(),m.end()));
+        }
+        printResult(stringJoiner);
+    }
+
+    private static void printResult(StringJoiner stringJoiner) {
+        if (!stringJoiner.toString().isBlank()){
+            System.out.println("Matches found: " + stringJoiner);
+        }else {
+            System.out.println("No matches");
         }
     }
 
     public static void main(String[] args) {
-        String usOccurrences = "$123.45$12.45.12.35$$8888.456";
-        printOccurrences(usOccurrences);
+        String usCurrency = printUsCurrency();
+        printOccurrences(usCurrency);
     }
 }
