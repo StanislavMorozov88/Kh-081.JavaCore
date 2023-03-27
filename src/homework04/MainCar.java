4f74ce3c97f42b218fa1a396baa1b4801a6a23f3
OlhenShu <olya.shutileva@gmail.com>
2023-03-27 14:48:52 +0300

    comments from Olha

--- src/homework04/MainCar.java
+++ src/homework04/MainCar.java
@@ -14,7 +14,23 @@ public class MainCar {
         cars[2] = new Car("Mitsubishi", 2003, 2.0F);
         cars[3] = new Car("Ford", 2010, 1.55F);
 
-        System.out.println("Enter the year: ");
+        //у тебе немає сортування, я додала
+        for (int i = 1; i < cars.length; i++) {
+            Car key = cars[i];
+            int j = i - 1;
+            while (j >= 0 && cars[j].getYear() < key.getYear()) { // or ">" for ASC oder
+                cars[j + 1] = cars[j];
+                j--;
+            }
+            cars[j + 1] = key;
+        }
+
+        // Print the sorted cars
+        for (Car car : cars) {
+            System.out.println(car.getType() + ", " + car.getYear() + ", " + car.getCapacity() + "cc");
+        }
+
+        System.out.println("Enter the model year: ");
         int yearNumber = Integer.parseInt(br.readLine());
 
         //System.out.println(Arrays.toString(cars));
--- src/homework04/Month.java
+++ src/homework04/Month.java
@@ -4,20 +4,25 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Month {
+public class Month {//better named class MonthDays
     public static void main(String[] args) throws Exception {
 
         int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        int n = 12;
+        int n = 12;//краще замість отакого робити перевірку
+        //в тому випадку у тебе виведеться повідомлення що невалідний номер місяця
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter month number (1-12): ");
         int monthNumber = Integer.parseInt(br.readLine());
 
 
-        if (monthNumber <= n) {
+//        if (monthNumber <= n) {
+//            System.out.println(monthNumber + " month has " + days[monthNumber - 1] + " days");
+//        }
+        if (monthNumber < 1 || monthNumber > 12) {
+            System.out.println("Invalid month number.");
+        } else   {
             System.out.println(monthNumber + " month has " + days[monthNumber - 1] + " days");
-        }
-
+       }
     }
 }
