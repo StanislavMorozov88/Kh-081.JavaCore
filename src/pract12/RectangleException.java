94c83703d2b083330b37992a0b6f9f1ca957614a
Yuliia Mokina <iuliia.mokina@gmail.com>
2023-04-01 15:49:36 +0300

    Little changes to pract12

--- src/pract12/RectangleException.java
+++ src/pract12/RectangleException.java
@@ -5,25 +5,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class RectangleException {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int squareRectangle = square();
         System.out.println("Rectangle square is: " + squareRectangle);
     }
 
-    public static int square() throws ArithmeticException {
+    public static int square() throws ArithmeticException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try {
             System.out.println("Enter value for a:");
             int a = Integer.parseInt(br.readLine());
             System.out.println("Enter value for b:");
             int b = Integer.parseInt(br.readLine());
-            if (a < 0 || b < 0) {throw new ArithmeticException();}
+            if (a < 0 || b < 0) {
+                throw new ArithmeticException();
+            }
             return a * b;
         } catch (NumberFormatException | IOException e) {
             return -1;
         } catch (Exception e) {
             return -2;
+        } finally {
+            br.close();
         }
+
     }
 }
 
