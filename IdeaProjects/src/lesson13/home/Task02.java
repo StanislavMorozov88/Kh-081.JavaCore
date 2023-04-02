08a717e5dba34f6975e6da432aa99691bd979c87
Olena Stankevych <yelenastankevych@gmail.com>
2023-04-03 00:57:26 +0300

    added home task 02 for lesson 13

---
+++ IdeaProjects/src/lesson13/home/Task02.java
@@ -0,0 +1,29 @@
+package lesson13.home;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
+
+public class Task02 {
+    static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM.dd.uuuu").withResolverStyle(ResolverStyle.STRICT);
+
+    public static boolean isValid(String str){
+        try {
+            LocalDate.parse(str,dateTimeFormatter);
+        }catch (DateTimeParseException e){
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String date1 = "03.10.1983";
+        System.out.println("Is valid date '" + date1 + "'?: " + isValid(date1));
+
+        String date2 = "13.02.2024";
+        System.out.println("Is valid date '" + date2 + "'?: " + isValid(date2));
+
+
+    }
+}
