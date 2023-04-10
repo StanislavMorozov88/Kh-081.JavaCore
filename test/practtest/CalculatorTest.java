91112bb0683e57c3c67baed5e307270333c70b7c
Yuliia Mokina <iuliia.mokina@gmail.com>
2023-04-10 11:28:14 +0300

    Add examples for tests

--- test/practtest/CalculatorTest.java
+++ test/practtest/CalculatorTest.java
@@ -1,4 +1,56 @@
+package practtest;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
-  
+
+//    @BeforeEach
+//    void setUp() {
+//        System.out.println("BeforeEach");
+//    }
+//
+//    @AfterEach
+//    void tearDown() {
+//        System.out.println("AfterEach");
+//    }
+
+    @Test
+    void add() {
+        int result = Calculator.add(2, 3);
+        assertEquals(5, result);
+    }
+    @Test
+    void addPositiveToNegative() {
+        int result = Calculator.add(2, -3);
+        assertEquals(-1, result);
+    }
+
+    @Test
+    void subtract() {
+        int result = Calculator.subtract(5, 4);
+        assertEquals(1, result);
+    }
+
+    @Test
+    void multiply() {
+        int result = Calculator.multiply(5, 4);
+        assertEquals(20, result);
+    }
+
+    @Test
+    void divide() {
+        int result = Calculator.divide(5, 4);
+        assertEquals(1, result);
+    }
+
+    @Test
+    void divideByZero() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Calculator.divide(6, 0);
+        });
+    }
 }
--- test/practtest/ReadConsoleTest.java
+++ test/practtest/ReadConsoleTest.java
@@ -1,4 +1,67 @@
-import static org.junit.jupiter.api.Assertions.*;
+package practtest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+/**У тестах використовуємо ByteArrayInputStream, щоб передати рядок "......" у System.setIn.
+ * Потім викликаємо readIntFromConsole і очікуємо, що він верне вміст але відповідного типу.
+ * Додатнє число в першому, від'ємне в другому, дабл в третьому тесті і т.д.
+ * Метод assertEquals перевіряє, що фактичний результат сповпадає з очікуваним.
+ * Якщо користувач вводить що-небудь інше окрім цілого, то метод scanner.nextInt() викликає виключення InputMismatchException.
+ */
 class ReadConsoleTest {
-  
+
+    @Test
+    void readNumberFromConsole() {
+        String input = "30";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        int result = ReadConsole.readNumberFromConsole();
+        assertEquals(30, result);
+    }
+    @Test
+    public void testReadIntFromConsoleNegativeNumber() {
+        String input = "-5";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertEquals(-5, ReadConsole.readNumberFromConsole());
+    }
+
+
+    @Test
+    public void testReadIntFromConsoleDecimalNumber() {
+        String input = "3.14";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        Assertions.assertThrows(InputMismatchException.class, () -> {
+            ReadConsole.readNumberFromConsole();
+        });
+    }
+    //зверніть увагу на параметри анотації
+    //    @Test(expected = InputMismatchException.class)//у мене так не працювало, тому переробила
+//    public void testReadIntFromConsoleEmptyInput() {
+//        String input = "\n";
+//        InputStream in = new ByteArrayInputStream(input.getBytes());
+//        System.setIn(in);
+//        ReadConsole.readNumberFromConsole();
+//    }
+    @Test
+    public void testReadIntFromConsoleEmptyInput() {
+        String input = "";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        Assertions.assertThrows(NoSuchElementException.class, ReadConsole::readNumberFromConsole);
+    }
+
+    @Test
+    public void testReadIntFromConsoleInvalidInput() {
+        String input = "some text";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        Assertions.assertThrows(InputMismatchException.class, () -> {
+            ReadConsole.readNumberFromConsole();
+        });
+    }
 }
