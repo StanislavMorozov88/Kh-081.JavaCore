93645adbc6c71c68c8657aafc84086a9ebcc8588
Yuliia Mokina <iuliia.mokina@gmail.com>
2023-04-10 11:26:55 +0300

    Small changes to MainDog (homework03)

--- src/homework03/MainDog.java
+++ src/homework03/MainDog.java
@@ -1,7 +1,10 @@
 package homework03;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class MainDog {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
 
         Dog d1 = new Dog("Fibi", "SCHNAUZER", 1);
@@ -11,6 +14,26 @@ public class MainDog {
         System.out.println(d1);
         System.out.println(d2);
         System.out.println(d3);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter a dog breed and get the name: ");
+        String code = br.readLine();
+
+        switch (DogBreed.valueOf(code)) {
+            case SCHNAUZER:
+                System.out.println(d1.getName());
+                break;
+            case DOBERMAN:
+                System.out.println(d2.getName());
+                break;
+            case DACHSHUND:
+                System.out.println(d3.getName());
+                break;
+            default:
+                System.out.println("Server error");
+                br.close();
+        }
 
         if (d1.getName().equals(d2.getName()) || (d3.getName().equals(d1.getName()))) {
             System.out.println("There are the same names of dogs");
